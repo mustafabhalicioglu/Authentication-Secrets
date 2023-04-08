e1b302e813fedea37cb63b549c0f104668d64779
mustafabhalicioglu <105492495+mustafabhalicioglu@users.noreply.github.com>
2023-04-08 10:42:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
+//if you took error message related the connection code below, you can try this: 
+  //mongoose.set("strictQuery", true);
+  //mongoose.connect('mongodb://127.0.0.1:27017/userDB');
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 mongoose.set("useCreateIndex", true);
 
